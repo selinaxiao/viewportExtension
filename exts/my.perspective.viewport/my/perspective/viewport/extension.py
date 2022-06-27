38f4497faa08cb8018c52c552baee3cac89da5d2
selina <yx229@cornell.edu>
2022-06-27 17:10:26 -0400

    first commit

---
+++ exts/my.perspective.viewport/my/perspective/viewport/extension.py
@@ -0,0 +1,215 @@
+import omni.ext
+import omni.ui as ui
+from omni.kit.widget.viewport import ViewportWidget
+
+from omni.ui import Workspace
+from  omni.kit.viewport.window.window import ViewportWindow
+from  omni.kit.viewport.window.dragdrop.usd_file_drop_delegate import UsdFileDropDelegate
+from  omni.kit.viewport.window.dragdrop.usd_prim_drop_delegate import UsdShadeDropDelegate
+from  omni.kit.viewport.window.dragdrop.material_file_drop_delegate import MaterialFileDropDelegate
+import carb
+DEFAULT_VIEWPORT_NAME = '/exts/my.perspective.vireport/startup/windowName'
+DEFAULT_VIEWPORT_NO_OPEN = '/exts/my.perspective.vireport/startup/disableWindowOnLoad'
+
+
+# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
+# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
+# on_shutdown() is called.
+class MyExtension(omni.ext.IExt):
+    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
+    # this extension is located on filesystem.
+    __all__ = ['ViewportExtension']
+
+    WINDOW_NAME = "Viewport Perspective"
+    MENU_PATH = f"Window/{WINDOW_NAME}"
+
+    def on_startup(self, ext_id):
+        print("[my.perspective.viewport] MyExtension startup")
+        # super().on_startup()
+
+        # self.viewport_window = omni.ui.Window('Perspective Viewport', width=1280, height=720+20) # Add 20 for the title-bar
+        # with self.viewport_window.frame:
+        #     with ui.VStack():
+        #         self.viewport_widget = ViewportWidget(resolution = (1280, 720))
+        #         ui.ComboBox(0,"Perspective","Orthographic", width=100, height=100)
+
+        #     # Control of the ViewportTexture happens through the object held in the viewport_api property
+        #     self.viewport_api = self.viewport_widget.viewport_api
+
+        #     # We can reduce the resolution of the render easily
+        #     self.viewport_api.resolution = (640, 480)
+
+        #     # We can also switch to a different camera if we know the path to one that exists
+        #     self.viewport_api.camera_path = '/World/Camera'
+
+        #     # And inspect 
+        #     # print(self.viewport_api.projection)
+        #     # print(self.viewport_api.transform)
+        
+
+        settings = carb.settings.get_settings()
+        default_name = settings.get(DEFAULT_VIEWPORT_NAME) or MyExtension.WINDOW_NAME
+        MyExtension.WINDOW_NAME = default_name
+        MyExtension.MENU_PATH = f'Window/{default_name}'
+
+        self.__window = None
+        self.__registered = None
+
+        open_window = not settings.get(DEFAULT_VIEWPORT_NO_OPEN)
+        Workspace.set_show_window_fn(MyExtension.WINDOW_NAME, lambda b: self.__show_window(None, b))
+        if open_window:
+            Workspace.show_window(MyExtension.WINDOW_NAME)
+            if self.__window:
+                MyExtension.dock_with_window(MyExtension.WINDOW_NAME, 'Viewport', omni.ui.DockPosition.SAME)
+        open_window = True if (open_window and self.__window) else False
+
+        editor_menu = omni.kit.ui.get_editor_menu()
+        if editor_menu:
+            self.__menu = editor_menu.add_item(MyExtension.MENU_PATH, self.__show_window, toggle=True, value=open_window)
+        
+        self.__registered = self.__register_scenes()
+        self.__default_drag_handlers = (
+            UsdFileDropDelegate('/persistent/app/viewport/previewOnPeek'),
+            UsdShadeDropDelegate(),
+            MaterialFileDropDelegate()
+        )
+
+
+    def on_shutdown(self):
+        print("[my.perspective.viewport] MyExtension shutdown")
+        Workspace.set_show_window_fn(MyExtension.WINDOW_NAME, None)
+        self.__show_window(None, False)
+        self.__menu = None
+        self.__default_drag_handlers = None
+        if self.__registered:
+            self.__unregister_scenes(self.__registered)
+            self.__registered = None
+
+        from omni.kit.viewport.window.events import set_ui_delegate
+        set_ui_delegate(None)
+
+    def dock_with_window(window_name: str, dock_name: str, position: omni.ui.DockPosition, ratio: float = 1):
+        async def wait_for_window():
+            dockspace = Workspace.get_window(dock_name)
+            window = Workspace.get_window(window_name)
+            if (window is None) or (dockspace is None):
+                frames = 3
+                while ((window is None) or (dockspace is None)) and frames:
+                    await omni.kit.app.get_app().next_update_async()
+                    dockspace = Workspace.get_window(dock_name)
+                    window = Workspace.get_window(window_name)
+                    frames = frames - 1
+
+            if window and dockspace:
+                window.deferred_dock_in(dock_name)
+                # This genrally works in a variety of cases from load, re-load, and save extesnion .py file reload
+                # But it depends on a call order in omni.ui and registered selected_in_dock and dock_changed callbacks.
+                await omni.kit.app.get_app().next_update_async()
+                updates_enabled = window.docked and window.selected_in_dock
+                window.viewport_api.updates_enabled = updates_enabled
+
+        import asyncio
+        asyncio.ensure_future(wait_for_window())
+
+    def __set_menu(self, value):
+        """Set the menu to create this window on and off"""
+        editor_menu = omni.kit.ui.get_editor_menu()
+        if editor_menu:
+            editor_menu.set_value(MyExtension.MENU_PATH, value)
+
+    
+    def __show_window(self, menu, visible):
+        self.__set_menu(visible)
+
+        if visible:
+            if not self.__window:
+                def visiblity_changed(visible):
+                    self.__set_menu(visible)
+                    if not visible:
+                        self.__show_window(None, False)
+                self.__window = ViewportWindow(MyExtension.WINDOW_NAME)
+                self.__window.set_visibility_changed_fn(visiblity_changed)
+        elif self.__window:
+            self.__window.set_visibility_changed_fn(None)
+            self.__window.destroy()
+            self.__window = None
+
+    def __register_scenes(self):
+        def _is_extension_loaded(extension_name: str) -> bool:
+            import omni.kit
+            def is_ext(ext_id: str) -> bool:
+                ext_name = ext_id.split("-")[0]
+                return ext_name == extension_name
+
+            for ext in omni.kit.app.get_app_interface().get_extension_manager().get_extensions():
+                if is_ext(ext['id']):
+                    return ext['enabled']
+            return False
+
+        # Register all of the items that use omni.ui.scene to add functionality
+        from omni.kit.viewport.registry import RegisterScene
+        from omni.kit.viewport.window.scene.scenes import SimpleGrid, SimpleOrigin, CameraAxis
+        registered = [
+            RegisterScene(SimpleOrigin, 'omni.kit.viewport.window.scene.SimpleOrigin'),
+            RegisterScene(CameraAxis, 'omni.kit.viewport.window.scene.CameraAxis')
+        ]
+
+        # Register items to control legacy drawing if it is loaded and available
+        if _is_extension_loaded('omni.kit.viewport.legacy_gizmos'):
+            from omni.kit.viewport.window.scene.legacy import LegacyGridScene, LegacyLightScene, LegacyAudioScene
+            registered += [
+                RegisterScene(LegacyGridScene, 'omni.kit.viewport.window.scene.LegacyGrid'),
+                RegisterScene(LegacyLightScene, 'omni.kit.viewport.window.scene.LegacyLight'),
+                RegisterScene(LegacyAudioScene, 'omni.kit.viewport.window.scene.LegacyAudio')
+            ]
+        else:
+            # Otherwise use omni.scene.ui Grid
+            registered += [
+                RegisterScene(SimpleGrid, 'omni.kit.viewport.window.scene.SimpleGrid')
+            ]
+
+        from omni.kit.viewport.window.manipulator.context_menu import ViewportClickFactory
+        registered += [
+            RegisterScene(ViewportClickFactory, 'omni.kit.viewport.window.manipulator.ContextMenu')
+        ]
+
+        from omni.kit.viewport.window.manipulator.object_click import ObjectClickFactory
+        registered += [
+            RegisterScene(ObjectClickFactory, 'omni.kit.viewport.window.manipulator.ObjectClick')
+        ]
+
+        # Register the Selection manipulator (if available)
+        from omni.kit.viewport.window.manipulator.selection import SelectionManipulatorItem
+        if SelectionManipulatorItem:
+            registered += [
+                RegisterScene(SelectionManipulatorItem, 'omni.kit.viewport.window.manipulator.Selection')
+            ]
+
+        # Register the Camera manipulator (if available)
+        from omni.kit.viewport.window.manipulator.camera import ViewportCameraManiulatorFactory
+        if ViewportCameraManiulatorFactory:
+            registered += [
+                RegisterScene(ViewportCameraManiulatorFactory, 'omni.kit.viewport.window.manipulator.Camera')
+            ]
+
+        from omni.kit.viewport.registry import RegisterViewportLayer
+
+        # Register the HUD stats
+        # XXX Can't do this in testing as the stats will not be consistent across launches
+        if not carb.settings.get_settings().get('/exts/omni.kit.test/runTestsAndQuit'):
+            from omni.kit.viewport.window.stats import ViewportStatsLayer
+            registered.append(RegisterViewportLayer(ViewportStatsLayer, 'omni.kit.viewport.window.ViewportStats'))
+
+        # Finally register the ViewportSceneLayer
+        from omni.kit.viewport.window.scene.layer import ViewportSceneLayer
+        registered.append(RegisterViewportLayer(ViewportSceneLayer, 'omni.kit.viewport.window.SceneLayer'))
+        return registered
+
+    def __unregister_scenes(self, registered):
+        for item in registered:
+            try:
+                item.destroy()
+            except Exception:
+                pass
+
+    
